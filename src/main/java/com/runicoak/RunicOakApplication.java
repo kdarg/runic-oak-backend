054e069c98ceedef548bb80e9389e43d6c8c08ef
Wyse- <5454041+Wyse-@users.noreply.github.com>
2023-03-12 19:22:25 +0100

    Spring Boot backend project setup

---
+++ src/main/java/com/runicoak/RunicOakApplication.java
@@ -0,0 +1,12 @@
+package com.runicoak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunicOakApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(RunicOakApplication.class, args);
+	}
+}
---
+++ src/test/java/com/runicoak/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runicoak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
